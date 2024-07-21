b91197cbe57dfa365e998b0ea9b806e8680213e5
Emlyn Corrin <emlyn.corrin@gmail.com>
2024-07-21 23:32:17 +0100

    Check if files are tracked by git

--- scripts/tasks.clj
+++ scripts/tasks.clj
@@ -1,7 +1,9 @@
 (ns tasks
   (:require [babashka.fs :as fs]
+            [babashka.process :as proc]
             [clojure.string :as str]))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn install-pre-commit
   [& {}]
   (let [src "scripts/pre-commit"
@@ -17,23 +19,27 @@
         (fs/copy src dst)
         (println "Pre-commit hook installed")))))
 
-(defn -check-file
-  [{:keys [src-suffix dst-suffixes check-modified verbose]} src-file]
-  (when verbose (println "Checking" src-file))
-  (let [stem (subs src-file 0 (- (count src-file) (count src-suffix)))
-        modified (fs/last-modified-time src-file)
-        dst-files (map #(str stem %) dst-suffixes)
-        missing (not-empty (filter (complement fs/exists?) dst-files))
-        outdated (when check-modified
-                   (not-empty (filter #(and (fs/exists? %)
-                                            (pos? (compare modified (fs/last-modified-time %))))
-                                      dst-files)))]
-    (when (or missing outdated)
-      (str "Source file " src-file
-           (when missing (str " missing " (str/join ", " missing)))
-           (when outdated (str " outdated " (str/join ", " outdated)))))))
+(defn- check-file
+  [{:keys [src-suffix dst-suffixes tracked? check-modified verbose]} src-file]
+  (if (not (tracked? src-file))
+    (when verbose (println "Skipping" src-file "(not tracked)"))
+    (do (when verbose (println "Checking" src-file))
+        (let [stem (subs src-file 0 (- (count src-file) (count src-suffix)))
+              modified (fs/last-modified-time src-file)
+              dst-files (map #(str stem %) dst-suffixes)
+              missing (not-empty (filter (complement fs/exists?) dst-files))
+              outdated (when check-modified
+                         (not-empty (filter #(and (fs/exists? %)
+                                                  (pos? (compare modified (fs/last-modified-time %))))
+                                            dst-files)))
+              untracked (not-empty (remove tracked? dst-files))]
+          (when (or missing outdated untracked)
+            (str "Source file " src-file
+                 (when missing (str " missing " (str/join ", " missing)))
+                 (when outdated (str " outdated " (str/join ", " outdated)))
+                 (when untracked (str " untracked " (str/join ", " untracked)))))))))
 
-(defn -check-dir
+(defn- check-dir
   [& {:keys [dir src-suffix verbose] :as args}]
   (when verbose (println "Listing" (str dir)))
   (let [{dirs true files false} (group-by fs/directory? (fs/list-dir dir))
@@ -41,23 +47,28 @@
                      (map str)
                      (filter #(str/ends-with? % src-suffix))
                      (sort)
-                     (map (partial -check-file args))
+                     (map (partial check-file args))
                      (remove nil?)
                      (vec))]
-    (reduce into results (map #(-check-dir (assoc args :dir %)) (sort dirs)))))
+    (reduce into results (map #(check-dir (assoc args :dir %)) (sort dirs)))))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn check
   {:org.babashka/cli {:coerce {:dst-suffixes [:str]
                                :check-modified :boolean
+                               :tracked-only :boolean
                                :verbose :boolean}
                       :validate {:dir fs/directory?}}}
-  [& {:keys [verbose] :as args}]
+  [& {:keys [dir tracked-only verbose] :as args}]
   (when verbose (println "Running check with" (pr-str args)))
-  (when-let [errors (not-empty (-check-dir args))]
-    (doseq [err errors]
-      (println "Error:" err))
-    (println)
-    (throw (ex-info "Errors found" {:errors errors}))))
+  (let [tracked (if tracked-only
+                  (-> (proc/sh "git" "ls-files" dir) :out str/split-lines set)
+                  (constantly true))]
+    (when-let [errors (not-empty (check-dir :tracked? tracked args))]
+      (doseq [err errors]
+        (println "Error:" err))
+      (println)
+      (throw (ex-info "Errors found" {:errors errors})))))
 
 (defn build-site
   [args]
